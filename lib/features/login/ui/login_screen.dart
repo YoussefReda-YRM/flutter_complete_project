86430336a5684f84cfd45e2521d5fd585093d74e
YoussefReda-YRM <yrda3414@gmai.com>
2024-06-23 18:54:01 +0300

    added development & production flavors for android, changed android app name for each flavor, added launch json for flavors config and  added two main files for each flavor

--- lib/features/login/ui/login_screen.dart
+++ lib/features/login/ui/login_screen.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_complete_project/core/theming/styles.dart';
 import 'package:flutter_complete_project/core/widgets/app_text_button.dart';
-import 'package:flutter_complete_project/features/login/data/models/login_request_body.dart';
 import 'package:flutter_complete_project/features/login/logic/cubit/login_cubit.dart';
 import 'package:flutter_complete_project/features/login/ui/widgets/dont_have_account_text.dart';
 import 'package:flutter_complete_project/features/login/ui/widgets/email_and_password.dart';
